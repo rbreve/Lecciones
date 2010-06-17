495195a0f27f8bb850c628c702ad856f4bea6443
Roberto Breve <rbreve@gmail.com>
2010-06-17 12:37:28 -0600

    env new gems versions

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,14 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'warden', :version => '0.10.3'
-  config.gem 'devise', :version => '1.0.6'
+  config.gem 'warden', :version => '0.10.7'
+  config.gem 'devise', :version => '1.0.7'
   config.gem 'formtastic', :lib => 'formtastic', :source => "http://gemcutter.org/"
    
   config.time_zone = 'UTC'
